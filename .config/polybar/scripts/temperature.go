80e81dfe95cda66d5f674bb98d4af9caddb71fc1
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-01-21 18:41:49 +0400

    upgraded custom temperature module

---
+++ .config/polybar/scripts/temperature.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+	"regexp"
+	"time"
+
+	"golang.org/x/sync/errgroup"
+)
+
+type temperature struct {
+	current   string
+	requested time.Time
+}
+
+func (t *temperature) listener(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		return
+	}
+
+	if r.FormValue("task") == "next" {
+		if t.current == "gpu" {
+			fmt.Println(t.cpu())
+		} else if t.current == "cpu" {
+			fmt.Println(t.hdd())
+		} else if t.current == "hdd" {
+			fmt.Println(t.gpu())
+		}
+	}
+}
+
+func (t *temperature) cur() {
+	if t.current == "gpu" {
+		fmt.Println(t.gpu())
+		t.requested = time.Now()
+	} else if t.current == "cpu" {
+		fmt.Println(t.cpu())
+		t.requested = time.Now()
+	} else if t.current == "hdd" {
+		fmt.Println(t.hdd())
+		t.requested = time.Now()
+	}
+}
+
+func (t *temperature) gpu() string {
+	out, err := exec.Command("sensors").Output()
+	if err != nil {
+		return err.Error()
+	}
+	pattern := regexp.MustCompile(`(?m)nouveau-pci-0600\n.*\n.*\ntemp1: +\+(?P<temp>\d+)\.`)
+	t.current = "gpu"
+	return "GPU +" + pattern.FindStringSubmatch(string(out))[1] + "°C"
+}
+
+func (t *temperature) cpu() string {
+	out, err := exec.Command("sensors").Output()
+	if err != nil {
+		return err.Error()
+	}
+	pattern := regexp.MustCompile(`(?m)k10temp-pci-00c3\n.*\nTdie: +\+(?P<temp>\d+)\.`)
+	t.current = "cpu"
+	return "CPU +" + pattern.FindStringSubmatch(string(out))[1] + "°C"
+}
+
+func (t *temperature) hdd() string {
+	out, err := exec.Command("nc", "localhost", "7634").Output()
+	if err != nil {
+		return err.Error()
+	}
+	pattern := regexp.MustCompile(`(?m)\|SAMSUNG.*\|(?P<temp>\d+)\|`)
+	t.current = "hdd"
+	return "HDD +" + pattern.FindStringSubmatch(string(out))[1] + "°C"
+}
+
+func main() {
+	temp := temperature{current: "gpu"}
+	temp.cur()
+	temp.requested = time.Now()
+
+	http.HandleFunc("/", temp.listener)
+
+	tasks, ctx := errgroup.WithContext(context.Background())
+
+	tasks.Go(func() error {
+		return http.ListenAndServe(":9096", nil)
+	})
+
+	tasks.Go(func() error {
+		tick := time.NewTicker(time.Second * 30)
+		defer tick.Stop()
+		for {
+			select {
+			case <-tick.C:
+				since := time.Since(temp.requested)
+				if since.Seconds() < 30 {
+					<-time.NewTimer(since).C
+					temp.cur()
+				} else {
+					temp.cur()
+				}
+			case <-ctx.Done():
+				return nil
+			}
+		}
+	})
+
+	if err := tasks.Wait(); err != nil {
+		log.Fatal("An error occurred:", err)
+	}
+}
