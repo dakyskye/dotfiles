91b8c6ae749939a0d2a1eaff106b1c38307d6b71
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-01-21 17:49:06 +0400

    added YouCompleteMe vim plugin

--- ensure_vim_plugins.sh
+++ ensure_vim_plugins.sh
@@ -10,3 +10,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git submodule init
 git submodule update
 
+cd ./.vim/bundle
+git submodule update --init --recursive
+python install.py --clang-completer --clangd-completer --ts-completer --go-completer
+
