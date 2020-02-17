2a4879cb241417cb6644f67c57865c046908b16d
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-02-18 00:13:06 +0400

    Made launch.sh able to launch different bars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,5 +7,5 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar mainbar &
+polybar "$1" &
 
