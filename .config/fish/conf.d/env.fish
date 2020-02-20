d0058391ab9d123a202fcd2bf05ac7d0dbf893b4
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-02-20 21:52:27 +0400

    added CONFIG env variable to fish

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -1,7 +1,9 @@
 set -gx TERM termite
 set -gx EDITOR nvim
 set -gx WORK $HOME/My-Workflow
+set -gx CONFIG $HOME/.config
 set -gx TOOLS $HOME/Tools
 set -gx GOPATH $HOME/go
 set -gx CARGOPATH $HOME/.cargo
 set -gx PATH $PATH $GOPATH/bin $CARGOPATH/bin $HOME/.yarn/bin  $TOOLS/bin
+
