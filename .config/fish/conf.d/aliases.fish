130908053b49e2a176957a800498ece0ebe3959d
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-02-27 14:47:57 +0400

    added aliases for exa

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,2 +1,10 @@
 alias vim="env SHELL=/usr/bin/bash nvim"
 
+alias ls="exa --color=always --group-directories-first"
+alias la="exa -a --color=always --group-directories-first"
+alias ll="exa -al --color=always --group-directories-first"
+alias lg="exa -alg --color=always --group-directories-first"
+alias lh="exa -alh --color=always --group-directories-first"
+alias lgh="exa -algh --color=always --group-directories-first"
+alias lt="exa -aT --color=always --group-directories-first"
+
