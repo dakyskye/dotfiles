24c455d5b0085421100e00e667f1802620767888
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-03-07 14:49:09 +0400

    replaced aliases with abbreviations

---
+++ .config/fish/conf.d/abbreviations.fish
@@ -0,0 +1,10 @@
+abbr vim "nvim"
+
+abbr ls "exa --color always --group-directories-first"
+abbr la "exa -a --color always --group-directories-first"
+abbr ll "exa -al --color always --group-directories-first"
+abbr lg "exa -alg --color always --group-directories-first"
+abbr lh "exa -alh --color always --group-directories-first"
+abbr lgh "exa -algh --color always --group-directories-first"
+abbr lt "exa -aT --color always --group-directories-first"
+
