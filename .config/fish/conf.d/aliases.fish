716cac239876b99164036b8f4be52a16fe6cd250
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-03-26 19:42:58 +0400

    replaced abbrs with aliases

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,9 @@
+alias ls="exa --color always --group-directories-first"
+alias la="exa -a --color always --group-directories-first"
+alias ll="exa -al --color always --group-directories-first"
+alias lg="exa -alg --color always --group-directories-first"
+alias lh="exa -alh --color always --group-directories-first"
+alias lgh="exa -algh --color always --group-directories-first"
+alias lt="exa -aT --color always --group-directories-first"
+
+alias rm="rm -i"
