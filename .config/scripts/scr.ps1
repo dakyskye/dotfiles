61dd0fe52e5591ba4b9824de58edddf62707e0f4
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-04-17 19:40:43 +0400

    made the scripts more portable

--- .config/scripts/scr.ps1
+++ .config/scripts/scr.ps1
@@ -5,6 +5,7 @@ $SS_Format = "$SS_Path/screenshot_$(date '+%d.%m.%Y_%H.%M.%S').png"
 $NOTIFY_Copy = "the screenshot has been taken and copied to your clipboard"
 $NOTIFY_Save = "the screenshot has been taken and saved locally at $SS_Path"
 $NOTIFY_Upload = "the link of captured screenshot has been copied to your clipboard"
+$NOTIFY_App_Name
 
 $FC = "fc"
 $WC = "wc"
@@ -19,12 +20,12 @@ function Screenshot ($type) {
 	}
 
 	Switch ($type) {
-		$FC { maim | xclip -selection clipboard -t image/png && notify-send "$NOTIFY_Copy" }
-		$WC { maim -sou | xclip -selection clipboard -t image/png && notify-send "$NOTIFY_Copy" }
-		$FS { maim "$SS_Format" && notify-send "$NOTIFY_Save" }
-		$WS { maim -sou "$SS_Format" && notify-send "$NOTIFY_Save" }
-		$FU { maim | curl -s -F 'file=@-' 'https://0x0.st' | xclip -selection clipboard && notify-send "$NOTIFY_Upload" }
-		$WU { maim -so | curl -s -F 'file=@-' 'https://0x0.st' | xclip -selection clipboard && notify-send "$NOTIFY_Upload" }
+		$FC { maim | xclip -selection clipboard -t image/png && notify-send -a "$NOTIFY_App_Name" "$NOTIFY_Copy" }
+		$WC { maim -sou | xclip -selection clipboard -t image/png && notify-send -a "$NOTIFY_App_Name" "$NOTIFY_Copy" }
+		$FS { maim "$SS_Format" && notify-send -a "$NOTIFY_App_Name" "$NOTIFY_Save" }
+		$WS { maim -sou "$SS_Format" && notify-send -a "$NOTIFY_App_Name" "$NOTIFY_Save" }
+		$FU { maim | curl -s -F 'file=@-' 'https://0x0.st' | xclip -selection clipboard && notify-send -a "$NOTIFY_App_Name" "$NOTIFY_Upload" }
+		$WU { maim -so | curl -s -F 'file=@-' 'https://0x0.st' | xclip -selection clipboard && notify-send -a "$NOTIFY_App_Name" "$NOTIFY_Upload" }
 	}
 }
 
