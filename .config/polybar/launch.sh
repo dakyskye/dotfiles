c43bb2a0f96426eb86bf84939e75de80410d253a
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2020-05-10 18:00:15 +0400

    modified the polybar launcher script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,29 +1,9 @@
 #!/bin/sh
 
-if [ -z "$1" ]; then
-	echo "no bar given" >&2
-	exit 1
-fi
-
 # terminate already running bar instances
 killall -q polybar
 wait $!
 
-CURRENT="$(wmctrl -m | awk '{if ($1 == "Name:") { print $2 }}')"
-if [ ! "$1" = "$CURRENT" ]; then
-	echo "can't launch $1 bar, whilst your current WM is $CURRENT" >&2
-	exit 1
-fi
+CURRENT="$(wmctrl -m | awk '{if ($1 == "Name:") { print tolower($2) }}')"
 
-launch_bar() {
-	polybar -c "$CONFIG"/polybar/config.ini -r "$1" &
-}
-
-case "$1" in
-	i3)
-		launch_bar i3
-		;;
-	*)
-		echo "can't launch $1 bar, it's not defined in $(dirname "$0")/launch.sh"
-		;;
-esac
+polybar -c "$CONFIG"/polybar/config.ini -r "$CURRENT"
