a38ab4a9214c4232efd1a0182438a7778f412ecb
dakyskye <kanteladzelasha339@gmail.com>
2020-09-13 20:26:01 +0400

    added new keybindings to dxhd config

--- dxhd/dxhd.sh
+++ dxhd/dxhd.sh
@@ -51,3 +51,11 @@ go run $SCRIPTS/wallpapers.go
 ## change volume
 # super + mouse{4,5}
 pamixer -{i,d} 1
+
+## launch rofimoji
+# super + shift + e
+rofimoji -c --skin-tone neutral
+
+## tells my s_temperature script to display next temperature
+# super + shift + t
+echo "next" | nc 127.0.0.1 8782
