1b9bb3ddfe8caae84d798ca95ae03f19f23a7999
dakyskye <kanteladzelasha339@gmail.com>
2020-11-28 13:59:01 +0400

    added fish config

---
+++ fish/config.fish
@@ -0,0 +1,22 @@
+# launch starship prompt
+starship init fish | source
+
+# aliases
+alias vim "nvim"
+alias spotify "spotify-tray"
+
+alias ls "exa --color always --group-directories-first"
+alias la "exa -a --color always --group-directories-first"
+alias ll "exa -al --color always --group-directories-first"
+alias lg "exa -alg --color always --group-directories-first"
+alias lh "exa -alh --color always --group-directories-first"
+alias lgh "exa -algh --color always --group-directories-first"
+alias lt "exa -aT --color always --group-directories-first -I .git"
+
+alias rm "rm -i"
+alias cp "cp -i"
+alias df "df -h"
+alias du "du -h"
+alias free "free -m"
+alias tb "nc termbin.com 9999"
+alias copy "xclip -selection clipboard"
