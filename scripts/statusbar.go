b2f987607042ce74c04d8fca1d9a8fa789739fda
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2021-01-02 17:39:56 +0400

    added monitor index to statsubar as I reconnected my other monitor

--- scripts/statusbar.go
+++ scripts/statusbar.go
@@ -58,7 +58,7 @@ func main() {
 	go sTime(errs, chanTime)
 
 	updateStatus := func() {
-		mon := fmt.Sprintf("%s | %s | %s | %s | %s | %s | %s | %s | %s", readMemory, readCPU, readTemperature, readUpdates, readWebcam, readVolume, readKeyboard, readTime, readDate)
+		mon := fmt.Sprintf("NIMDOW_MONITOR_INDEX=0 %s | %s | %s | %s | %s | %s | %s | %s | %s", readMemory, readCPU, readTemperature, readUpdates, readWebcam, readVolume, readKeyboard, readTime, readDate)
 		err = exec.Command("/usr/bin/xsetroot", "-name", mon).Run()
 		if err != nil {
 			errs <- err
