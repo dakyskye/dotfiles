bb412e28117d9a02aed99c01711705889d3ca235
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2021-09-01 17:46:56 +0400

    fixed volume reporting script

--- statusbar/scripts/volume.sh
+++ statusbar/scripts/volume.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 print_vol() {
-	PA_SINK="$(pacmd info | grep "Default sink name:" | awk '{ print $4; }')"
+	PA_SINK="$(pactl info | grep "Default Sink" | awk '{ print $3; }')"
 	SINK_INFO="$(pactl list sinks | grep -m1 -A 8 $PA_SINK)"
 
 	VOL="$(echo "$SINK_INFO" | grep "Volume:" | awk '{ print $5; }')"
