edc7bc05305aeebd2673280e4b1ec7bd80ede348
Lasha Kanteladze <kanteladzelasha339@gmail.com>
2025-11-11 22:36:44 +0400

    single esc to close telescope menu

--- nvim/lua/telescope_cfg.lua
+++ nvim/lua/telescope_cfg.lua
@@ -1,5 +1,17 @@
+local actions = require("telescope.actions")
+require("telescope").setup({
+	defaults = {
+		mappings = {
+			i = {
+				["<esc>"] = actions.close,
+			},
+		},
+	},
+})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
